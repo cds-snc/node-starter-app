698d64e66b735ba220ec28394e0dea2b83d66bad
Tim Arney <tim@line37.com>
2019-09-13 13:57:56 -0400

    move package

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = (env, argv) => {
-  const { getConfig } = require('node-starter-demo')
+  const { getConfig } = require('@cds-snc/webpack-starter')
 
   return (config = getConfig({
     entry: {
