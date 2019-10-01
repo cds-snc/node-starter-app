0947ccaf4ab889f671ff8021a6fb701f017ab79c
Tim Arney <tim@line37.com>
2019-10-01 10:03:03 -0400

    add aws account to env

--- cdk/bootstrap.js
+++ cdk/bootstrap.js
@@ -1,16 +1,25 @@
 // import environment variables.
 require('dotenv').config({ path: '../.env' })
 
-const exec = require("child_process").exec;
-const path = require('path');
+const exec = require('child_process').exec
+const path = require('path')
 
-process.env.AWS_SHARED_CREDENTIALS_FILE = path.resolve(__dirname, '../credentials')
+process.env.AWS_SHARED_CREDENTIALS_FILE = path.resolve(
+  __dirname,
+  '../credentials',
+)
 
-exec(`cdk bootstrap aws://241785157803/ca-central-1 -v`, {env: process.env}, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  console.log(`stderr: ${stderr}`);
-})
+const awsAccountId = process.env.AWS_ACCOUNT_ID
+
+exec(
+  `cdk bootstrap aws://${awsAccountId}/ca-central-1 -v`,
+  { env: process.env },
+  (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`)
+      return
+    }
+    console.log(`stdout: ${stdout}`)
+    console.log(`stderr: ${stderr}`)
+  },
+)
