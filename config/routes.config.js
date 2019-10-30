f55fffcdaea311f9ff6a692668da29a426dc8f8f
MarcoGoC <marco.flores@hrsdc-rhdcc.gc.ca>
2019-10-30 19:37:36 +0000

    added translation to registration page and title in french

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,7 +1,7 @@
 // 1) add your route(s) here ⬇️
 const routes = [
   { name: 'start', path: { en: '/start', fr: '/debut' } },
-  { name: 'registration', path: { en: '/registration' } },
+  { name: 'registration', path: { en: '/registration', fr: '/enregistrement' } },
   { name: 'confirmation', path: '/confirmation' },
   { name: 'landing', path: '/landing' },
 ]
