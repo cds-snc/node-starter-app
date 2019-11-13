62d0c9dc55ef3217158d00fd5a8fb6eddcd2a21d
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-11-13 12:21:50 -0500

    add purgecss to cleanup unused css

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,17 @@
+// postcss.config.js
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project
+  content: ['./routes/**/*.njk', './views/**/*.njk'],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+})
+
 module.exports = {
   plugins: [
     require('@csstools/postcss-sass'),
     require('tailwindcss'),
     require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
   ],
 }
