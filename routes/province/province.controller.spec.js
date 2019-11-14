c2fd98ec7bd32427f7c2cc715eb5525a107b36ce
Glenn <gmercier1986@hotmail.com>
2019-11-14 09:38:43 -0500

    fixedTest

--- routes/province/province.controller.spec.js
+++ routes/province/province.controller.spec.js
@@ -2,7 +2,7 @@ const request = require('supertest')
 const app = require('../../app.js')
 
 test('Can send get request landing route ', async () => {
-  const route = app.routes.get('landing')
+  const route = app.routes.get('start')
   const response = await request(app).get(route.path.en)
   expect(response.statusCode).toBe(200)
 })
