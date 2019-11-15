5623b305db0276dfe10e5f9a5ee5d1d648719c38
Glenn <gmercier1986@hotmail.com>
2019-11-15 13:52:24 -0500

    fixed notification

--- routes/registration/schema.js
+++ routes/registration/schema.js
@@ -31,44 +31,15 @@ const Schema = {
       errorMessage: 'errors.confirmEmail',
     },
   },
-  // expiry: {
-  //   customSanitizer: {
-  //     options: value => {
-  //       // We want to remove any spaces, dash or underscores
-  //       return value ? value.replace(/[_]*/g, '') : value
-  //     },
-  //     errorMessage: 'errors.expiry.date.format',
-  //   },
-  //   custom: {
-  //     options: (value, { req }) => {
-  //       return isValidDate(value)
-  //     },
-  //     errorMessage: 'errors.expiry.date',
-  //   },
-  // },
+
   accessible: {
     isIn: {
       errorMessage: 'errors.accessible.valid',
       options: [['Yes', 'No']],
     },
   },
-  notify_type: {
-    custom: {
-      options: (value, { req }) => {
-        const sendNotifications = req.body.accessible
-        if (sendNotifications && sendNotifications === 'Yes') {
-          if (typeof value === 'undefined') {
-            return false
-          }
-        } else {
-          req.body.notify_type = undefined
-        }
+  
 
-        return true
-      },
-      errorMessage: 'errors.notify_type',
-    },
-  },
 }
 
 module.exports = {
