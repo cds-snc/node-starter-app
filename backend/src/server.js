9150329cbe7acc27f103eda1c5ea31d17051d129
will0684 <jordan.walcaraz@gmail.com>
2019-11-18 13:04:19 -0500

    Added basic express config

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,26 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import helmet from 'helmet'
+import nosniff from 'dont-sniff-mimetype'
+import cors from 'cors'
+import morgan from 'morgan'
+
 require('dotenv').config()
+
+const app = express()
+const port = 3000
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(helmet())
+  app.disable('x-powered-by')
+  app.use(nosniff())
+}
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+)
 
 console.log(process.env.ENV_TEST)
