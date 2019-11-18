6f3cbfde2df7fd3316fd6c67bafa4ced75178683
will0684 <jordan.walcaraz@gmail.com>
2019-11-18 13:10:23 -0500

    Added more default configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,27 @@
-import express from 'express'
-import bodyParser from 'body-parser'
 import helmet from 'helmet'
 import nosniff from 'dont-sniff-mimetype'
+import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
-
+import path from 'path'
+import bodyParser from 'body-parser'
 require('dotenv').config()
 
 const app = express()
-const port = 3000
 
+// Helmet options for production environment
 if (process.env.NODE_ENV === 'production') {
   app.use(helmet())
   app.disable('x-powered-by')
   app.use(nosniff())
 }
 
+// Logging for request details
+process.env.NODE_ENV === 'development'
+  ? app.use(morgan('dev'))
+  : app.use(morgan('combined'))
+
+// Parser for request handlers
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
@@ -23,4 +29,22 @@ app.use(
   }),
 )
 
-console.log(process.env.ENV_TEST)
+// Enable CORS for all requests
+app.use(cors())
+app.options('*', cors())
+
+// Handler for 404 - resources not found
+app.use((req, res) => {
+  res.status(404).send('we think you are lost')
+})
+
+// Handler for Error 500
+app.use((err, req, res) => {
+  console.error(err.stack)
+  res.sendFile(path.join(__dirname, '../public/500.html'))
+})
+
+// Server port
+const PORT = process.env.PORT || 4001
+
+app.listen(PORT, () => console.info(`Server has started on ${PORT}`))
