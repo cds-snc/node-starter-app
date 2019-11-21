dd400e706c743428f2321c01d78235f42edf277b
Tim Arney <tim@line37.com>
2019-11-21 09:03:20 -0500

    cleanup

--- routes/confirmation/confirmation.controller.spec.js
+++ routes/confirmation/confirmation.controller.spec.js
@@ -23,6 +23,5 @@ jest.mock('../../utils/session.helpers', () => {
 test('Confirmation receives 200 when data exists', async () => {
   const route = app.routes.get('confirmation')
   const response = await request(app).get(route.path.en)
-  expect(response.text).toContain('My full name')
   expect(response.statusCode).toBe(200)
 })
