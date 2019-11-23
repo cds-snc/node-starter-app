f6f346d1a92de4c6d1a0274ca833b66334afed7e
http://jneen.net/ <jneen@jneen.net>
2019-11-22 19:23:29 -0500

    use the new style controls in the personal route

--- routes/personal/personal.controller.js
+++ routes/personal/personal.controller.js
@@ -2,14 +2,8 @@ const { routeUtils, getClientJs } = require('./../../utils')
 const { Schema } = require('./schema.js')
 
 module.exports = (app, route) => {
-  route
-    .draw(app)
-    .get((req, res) => {
-      const js = getClientJs(req, route.name)
-      res.render(
-        route.name,
-        routeUtils.getViewData(req, { jsFiles: js ? [js] : false }),
-      )
-    })
+  route.draw(app)
+    .use(route.loadSchema(Schema))
+    .get(route.render())
     .post(route.applySchema(Schema), route.doRedirect())
 }
