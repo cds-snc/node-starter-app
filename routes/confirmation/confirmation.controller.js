0acf3d70bb3a0699966914d04e5cdbae43772efa
http://jneen.net/ <jneen@jneen.net>
2019-11-22 19:21:47 -0500

    use loadFullSession in the confirmation page

--- routes/confirmation/confirmation.controller.js
+++ routes/confirmation/confirmation.controller.js
@@ -6,16 +6,5 @@ const {
 
 module.exports = (app, route) => {
   route.draw(app)
-    .get(async (req, res) => {
-      // ⚠️ experimental
-      // validate data from previous step
-      // see if we should be allowed to reach this step
-      const { Schema } = require('../personal/schema.js')
-      const result = await validateRouteData(req, Schema)
-      if (!result.status) {
-        setFlashMessageContent(req, result.errors)
-      }
-
-      res.render(route.name, getViewData(req))
-    })
+    .get(route.loadFullSession(), route.render())
 }
