ef0f025c391d43f98f3142e2f367b6a95af55cf2
http://jneen.net/ <jneen@jneen.net>
2019-12-09 12:48:29 -0500

    fix and simplify the logic of details-polyfill-detect

--- assets/js/details-polyfill-detect.js
+++ assets/js/details-polyfill-detect.js
@@ -1,10 +1,5 @@
-const needsPolyfill = () => {
-  if (typeof Promise === 'function') return false
-  if (document.querySelector('details') !== null) return false
-  return true
-}
-
-if (needsPolyfill()) {
+if (typeof Promise !== "function" &&
+    document.querySelector('details') !== null) {
   const script = '<script src="/js/details-element-polyfill.js"><\/script>'
   document.write(script) // lgtm[js/eval-like-call]
 }
