0e9ac7e53bfe4a05dcfb57958a85c6631c57eec6
http://jneen.net/ <jneen@jneen.net>
2020-03-06 16:12:38 -0500

    add integration tests and integrate the a11y-tracker

--- config/cypress.support.js
+++ config/cypress.support.js
@@ -1,23 +1,21 @@
-import { A11yReporter } from '@cdssnc/a11y-tracker-client'
+import { A11yReporter } from './../../a11y-tracker-client'
 
 const nodeEnv = Cypress.env('NODE_ENV') || 'development'
 
-if (nodeEnv === 'development') {
-  A11yReporter.setupCypress({
-    baseURI: undefined, // no reporting in development!
-    runId: '<local>',
+// default to not reporting
+A11yReporter.configure({
+  trackerURI: undefined,
+  revision: '<local>',
+  project: 'node-starter-app',
+})
+
+// if we're in CI and on the master branch, do the actual reporting
+if (Cypress.env['CI'] && Cypress.env['GITHUB_REF'] === 'refs/heads/master') {
+  A11yReporter.configure({
+    tracker: 'https://a11y-tracker.digital.canada.ca',
+    revision: Cypress.env('GITHUB_GIT_HASH'),
+    key: Cypress.env('A11Y_TRACKER_KEY')
   })
-}
-else if (nodeEnv === 'staging') {
-  A11yReporter.setupCypress({
-    baseURI: 'https://a11y-tracker.digital.canada.ca',
-    runId: Cypress.env('GITHUB_GIT_HASH'),
-  })
-}
-else {
-  throw new Error(`cannot test in environment: ${nodeEnv}`)
 }
 
-afterEach(() => {
-  cy.reportA11y()
-})
+A11yReporter.setupCypress()
--- routes/personal/integration.js
+++ routes/personal/integration.js
@@ -1,6 +1,14 @@
 describe('personal', () => {
-  it('has a button', () => {
-    cy.visit('/en/personal')
+  beforeEach(() => cy.visit('/en/personal'))
+
+  it('has a title', () => {
     cy.contains("Personal Information").should('be.visible')
+    cy.reportA11y()
+  })
+
+  it('shows errors', () => {
+    cy.get('form').submit()
+    cy.contains('errors').should('be.visible')
+    cy.reportA11y()
   })
 })
